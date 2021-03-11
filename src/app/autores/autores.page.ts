d6b8e7ced7960df6354bea9598bc6ca5ff28bbc0
Carol <atendimento@zaniniinfo.com.br>
2021-03-10 22:02:06 -0300

    iniciado listagem de autores

--- src/app/autores/autores.page.ts
+++ src/app/autores/autores.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Autor } from './autor_model';
+import { Genero } from './genero.enum';
 
 @Component({
   selector: 'app-autores',
@@ -7,7 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AutoresPage implements OnInit {
 
-  constructor() { }
+  autores: Autor[];
+
+  constructor() { 
+    this.autores = [
+      {nome: 'David Flanagan',
+      dataNascimento: new Date(1925,7,26),
+      genero: Genero.MASCULINO
+      },
+      {nome: 'Douglas Crockford',
+      dataNascimento: new Date(1955,5,25),
+      genero: Genero.MASCULINO
+      },
+      {nome: 'Martin Fowler',
+      dataNascimento: new Date(1963,11,18),
+      genero: Genero.MASCULINO
+      }
+    ];
+  }
 
   ngOnInit() {
   }
